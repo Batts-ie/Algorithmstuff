2e897ff422d1f5e67e8c4974af8dae095c842e12
Badrillox <micperktold@tsn.at>
2020-11-04 12:05:15 +0100

    Endrekursive Methode von Fibonacci

--- EndrecursiveFibonacci/EF.java
+++ EndrecursiveFibonacci/EF.java
@@ -6,12 +6,12 @@ public class EF {
 	public static void main(String[] args)
 	{
 		System.out.println("Geben Sie eine Zahl ein: ");
-		_zahl = scanner.nextDouble(); 
+		_zahl = Reader.nextDouble(); 
 		System.out.println(fibonacci(n));
 	}
 	public static int eF(double zahl, int fbn1, int fbn2)
 	{
-		if(n <= 1) return fbn1 + fbn2;
+		if(zahl <= 1) return fbn1 + fbn2;
 		else
 		{
 			return eF(zahl-1, fbn2+fbn1, fbn1);
