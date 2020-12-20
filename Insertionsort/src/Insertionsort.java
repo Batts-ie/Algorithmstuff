fd847bea2550f024c66edc858375e5460260a6d0
Badrillox <micperktold@tsn.at>
2020-12-20 14:04:49 +0100

    creating Insertionsort

---
+++ Insertionsort/src/Insertionsort.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Insertionsort 
+{
+    static Scanner Reader = new Scanner(System.in);
+    public static void main(String[] args) 
+    {
+        
+    }
+
+    public void Sort(int[] arr)
+    {
+
+    }
+}
